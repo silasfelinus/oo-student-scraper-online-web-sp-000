468a4e401dd8233787ee7723a655ddb9bb7a9fee
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-21 07:27:58 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,14 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = Nokogiri::HTML(URI.open(index_url)).css("div")#roster-cards-container" "student-card")
+    binding.pry
+
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
