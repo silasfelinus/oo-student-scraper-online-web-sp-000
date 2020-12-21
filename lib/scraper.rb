7196ed5a52ee67a2908d28f05e4b457856457996
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-21 23:04:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,8 +9,7 @@ class Scraper
     Nokogiri::HTML(URI.open(index_url)).css("div.roster-cards-container").css("div.student-card").each do |student|
       student_name =  student.css("div.card-text-container").css("h4.student-name").text
       student_location = student.css("div.card-text-container").css("p.student-location").text
-      student_profile_url = student.css("a")[0]
-      binding.pry
+      student_profile_url = student.css("a")[0]["href"]
       student_hash = {:name => student_name, :location => student_location, :profile_url => student_profile_url}
       student_array << student_hash
     end
