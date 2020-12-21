f1ede4430f6ddb4ce35f603349c94c712968fb8b
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-21 22:11:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,7 @@ class Scraper
       student_hash = {"name" => student_name, "location" => student_location}
       student_array << student_hash
     end
-    binding.pry 
+    binding.pry
     student_array
   end
 
