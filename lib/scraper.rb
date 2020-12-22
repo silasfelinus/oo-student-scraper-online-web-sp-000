e35435b6aa357e6d67cd97279433c59bf3f980ec
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-22 00:55:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,7 +26,7 @@ class Scraper
     student_blog = ""
 
     #Work throguh array of social media urls
-    student_urls = student_data.css("div.vitals-container").css("div.social-icon-container").css("a").each do |link|
+    student_urls = student_data.css("div.vitals-container").css("div.social-icon-container").css("a")["href"].each do |link|
       binding.pry
     end
     if student_urls.css("a")[0]
