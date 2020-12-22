8eb5d1cabf3e9ffbedc4eda7d6f7352cb2830e6d
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-22 03:22:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,15 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @name = student_hash[:name]
-    @location = student_hash[:location]
-    @twitter = student_hash[:twitter]
-    @linkedin = student_hash[:linkedin]
-    @github = student_hash[:github]
-    @blog = student_hash[:blog]
-    @profile_quote = student_hash[:profile_quote]
-    @bio = student_hash[:bio]
-    @profile_url = student_hash[:profile_url]
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
 
     @@all << self
 
