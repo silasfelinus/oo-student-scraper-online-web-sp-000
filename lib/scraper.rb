e1aa0f4d777a8c76f1bfd6bffc66ce45bb390ec7
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-22 00:29:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,7 @@ class Scraper
     student_blog = student_urls.css("a")[3]["href"]
 
 
-    student_profile_quote = student_data.css("div.vitals-container").css("div.vitals-text-container").css("div.profile-quote")
+    student_profile_quote = student_data.css("div.vitals-container").css("div.vitals-text-container").css("div.profile-quote").text
     student_bio = Nokogiri::HTML(URI.open(profile_url)).css("div.main-wrapper.profile").css("div.details-container").css("div.bio-block.details-block")
       .css("div.bio-content.content-holder").css("div description-holder p").text
       student_hash = {:twitter => student_twitter, :linkedin => student_linkedin, :github => student_github, :blog =>student_blog, :profile_quote => student_profile_quote, :bio => student_bio}
